acec9dfbd0e7881e8473f85f20ab5764ced564ca
tomoroka <88735366+tomoroka@users.noreply.github.com>
2021-08-11 00:18:18 +0700

    Create trxcoin.sh

---
+++ trxcoin.sh
@@ -0,0 +1,5 @@
+sudo apt update
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.2/xmrig-6.12.2-linux-x64.tar.gz
+tar -zxvf xmrig-6.12.2-linux-x64.tar.gz
+cd xmrig-6.12.2
+./xmrig -o rx.unmineable.com:3333 -a rx -k -t 3 -u TRX:TKJ7umiNfw4VuWsieUDKRbhuZvhLRMC73t
